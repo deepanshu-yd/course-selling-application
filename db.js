4d3a2f599f1fbf832f2b55309ce21ed24e26e01d
deepanshu <yadavdeepanshu000@gmail.com>
2025-08-22 15:32:23 +0530

    added the db connection string with the use of dotenv

--- db.js
+++ db.js
@@ -1,22 +1,43 @@
-const { Schema, default: mongoose } = require("mongoose");
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const ObjectId = mongoose.Types.ObjectId;
+mongoose.connect(process.env.MONGO_URI);
 
 const userSchema = Schema({
-
+    email: { type: String, unique: true },
+    password: String,
+    firstName: String,
+    lastName: String,
 });
 
 const adminSchema = Schema ({
-
+    email: { type: String, unique: true },
+    password: String,
+    firstName: String,
+    lastName: String,
 });
 
 const courseSchema = Schema ({
-
+    title: String,
+    description: String,
+    price: Number,
+    imageUrl: String,
+    creatorId: ObjectId
 });
 
 const purchaseSchema = Schema({
-
+    userId: ObjectId,
+    creatorId: ObjectId
 });
 
 const userModel = mongoose.Model("user", userSchema);
-const adminModel = mongoose.Model("Model", userSchema);
-const courseModel = mongoose.Model("course", userSchema);
-const purchaseModel = mongoose.Model("purchase", userSchema);
+const adminModel = mongoose.Model("Model", adminSchema);
+const courseModel = mongoose.Model("course", courseSchema);
+const purchaseModel = mongoose.Model("purchase", purchaseSchema);
+
+module.exports = {
+    userModel,
+    adminModel,
+    courseModel,
+    purchaseModel
+}
